0504484f62d1d77eff77b92a1fe438e71edb76b1
Sudesmer <sudesmer001@gmail.com>
2026-01-18 13:30:52 +0300

    fix: PrismaService providers'a eklendi - backend crash düzeltildi

--- backend/src/highlights/highlights.module.ts
+++ backend/src/highlights/highlights.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { HighlightsController } from './highlights.controller';
 import { HighlightsService } from './highlights.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   imports: [PrismaModule],
   controllers: [HighlightsController],
-  providers: [HighlightsService],
+  providers: [HighlightsService, PrismaService],
   exports: [HighlightsService],
 })
 export class HighlightsModule {}
